08a6eca5f3c100786e3b2915401d67386d5be91b
Savas Karakus <savas.kara21@gmail.com>
2018-07-22 22:01:20 -0700

    Recitation 2 notes

---
+++ rec2.py
@@ -0,0 +1,49 @@
+## Recitation 2 code
+
+# What is the decrementing function?
+# What does this loop do?
+
+### Prints even numbers from 2 to 10
+# a = 2
+# while (a <= 10) :
+#     a += 2
+#     print (a)
+
+# odd_number = 1
+# while (odd_number <= 10):
+#     odd_number += 2
+#     print(odd_number)
+
+#
+##For loop
+#
+
+# for i in (2,4,6,8,10):
+#     print(i)
+
+### Kind of inconvenient
+
+#
+### RAnge Function
+#
+
+#first paratmeter is inclusive and second parameter is exclusive
+# print (range(100))
+# print (range(1,100))
+# print(range(1,100,2))
+# count down
+# print(range(100,1,-1))
+#how to check the built in functions 
+# bracets are used for optional parameters
+# help(range)
+
+# for i in range(2,1001,2):
+#     print(i)
+
+
+#
+#Tuple
+#
+
+tuple_of_numbers = (3.1432, 2, 1, -100)
+tuple_of_strings = ("WHat", "is", "tupleps")
